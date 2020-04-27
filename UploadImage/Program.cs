7a0c7c79c5781b279fd0b1cbcec7c362c6f35836
Home <fengints@yandex.ru>
2020-04-27 13:37:17 +0600

    tr

--- UploadImage/Program.cs
+++ UploadImage/Program.cs
@@ -1,9 +1,11 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -22,5 +24,6 @@ namespace UploadImage
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+    
     }
 }
